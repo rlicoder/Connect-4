ee31468a55f79aead1a9d973805aeb55d57085ec
TieWeitong_CIS17A_40366 <tie04021030@gmail.com>
2021-03-21 18:39:32 -0700

    Update main.cpp

--- connect4_user/main.cpp
+++ connect4_user/main.cpp
@@ -11,17 +11,23 @@ struct UserInfo
 	char password[20];
 };
 
+
 //new user register
-int regest(struct UserInfo* user){
+void regest(){
 	char username[20];
 	char password[20];
-	strcpy(username, user->username);
-	strcpy(password, user->password);
 	string temp;
 	int flag = 0;
 	int i = 0;
+        int j = 0;
 	ifstream in("UserInfo.txt", ios::in);
 	ofstream out("UserInfo.txt", ios::app);
+        
+        cout << "Enter user name:\n";
+        cin >> username;
+        cout << "Enter password: \n";
+        cin >> password;
+        
 	while (getline(in, temp))//read data and save to temp
 	{
 		i++;
@@ -34,25 +40,39 @@ int regest(struct UserInfo* user){
 	}
 	in.close();
 	if (flag){
-		return 0;
+		j = 0;
 	}
 	else{
 		out << username << endl;
 		out << password << endl;
 		out.close();
-		return 1;
+		j = 1;
 	} // write username and password
+        switch (j)
+	{
+	case 1:
+		cout << "Successfully registered." << endl;
+	case 0:
+		cout << "The username has been registered, please select another username." << endl;
+	default:
+            return;
+	}
 }
-int login(struct UserInfo* user){
+void login(){
 	char username[20];
 	char password[20];
-	strcpy(username, user->username);
-	strcpy(password, user->password);
+
 	string temp1;
 	string temp2;
 	int existName = 0;
 	int match = 0;
 	int i = 0;
+        int j = 0;
+        
+        cout << "Enter your username: \n";
+        cin >> username;
+        cout << "Enter your password: \n";
+        cin >> password;
 	ifstream in("UserInfo.txt", ios::in);
 	while (getline(in, temp1))
 	{
@@ -67,50 +87,97 @@ int login(struct UserInfo* user){
 	}
 	in.close();
 	if (!existName){
-		return 2;//user name does not exist
+		j = 2;//user name does not exist
 	}
 	else{
 		if(match){
-                    return 1;
+                    j = 1;
                 }
 		else{
-                    return 3;
+                    j = 3;
+                }
+	}
+        switch (j)
+	{
+            case 1:
+		cout << "Successfully login in." << endl;
+            case 2:
+		cout << "The username does not exit." << endl;
+            case 3:
+                cout << "Wrong password." << endl;
+            default:
+                return;
+	}
+        
+}
+void Edit(){
+    	char username[20];
+	char password[20];
+        char newPassword[20];
+	string temp1;
+	string temp2;
+        int existName = 0;
+	int match = 0;
+	int i = 0;
+        int j = 0;
+	ifstream in("UserInfo.txt", ios::in);
+	ofstream out("UserInfo.txt", ios::app);
+        
+        cout << "Enter your username: \n";
+        cin >> username;
+        cout << "Enter your password: \n";
+        cin >> password;
+	while (getline(in, temp1))
+	{
+		getline(in, temp2);//read username and password
+		if (!strcmp(username, temp1.c_str())){
+			existName = 1;
+			if (!strcmp(password, temp2.c_str())){
+				match = 1;
+				break;
+			}
+		}//check the username if it is exist then compare the password
+	}
+        if (!existName){
+		j = 2;//user name does not exist
+	}
+	else{
+		if(match){
+                    cout << "Please enter your new password:" << endl;
+                    cin >> newPassword;
+                    password == newPassword;
+                    out << password << endl;    
+                }
+		else{
+                    cout << "Wrong password, please login again." << endl;
                 }
 	}
 }
+void menu(){
+    
+    UserInfo info;
+    int c;
+    
+    cout << "1.Register.\n";
+    cout << "2.login in.\n";
+    cout << "3.Change new password.\n";
+    cin >> c;
+    
+    switch(c){
+        case 1:
+            regest();
+        case 2:
+            login();
+        case 3:
+            Edit();
+        default:
+            break;
+    }
+}
 int main()
 {
-	UserInfo test1;
-	strcpy(test1.username, "admin");
-	strcpy(test1.password, "password");
-	switch (regest(&test1))
-	{
-	case 1:
-		cout << "Successfully registered." << endl;
-		break;
-	case 0:
-		cout << "The username has been registered, please select another username." << endl;
-		break;
-	default:
-		break;
-	}
- 
-	UserInfo test2;
-	strcpy(test2.username, "admin");
-	strcpy(test2.password, "password");
-	switch (login(&test2))
-	{
-	case 1:
-		cout << "Logging in...." << endl;
-		break;
-	case 3:
-		cout << "Wrong password, please login again." << endl;
-		break;
-	case 2:
-		cout << "The username was not found." << endl;
-		break;
-	default:
-		break;
-	}
-	return 0;
+    menu();
+
+    return 0;
+    
 }
