0af72cd1a4996c769e89f1d4b3e745c5edbfe8f7
TieWeitong_CIS17A_40366 <tie04021030@gmail.com>
2021-06-02 01:35:19 -0700

    Create connect4_database.sql

---
+++ connect4_database.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: connect4
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.18-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entity_user`
+--
+
+DROP TABLE IF EXISTS `entity_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entity_user` (
+  `user_name` varchar(50) NOT NULL,
+  `pass` varchar(50) DEFAULT NULL,
+  `email` tinytext DEFAULT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entity_user`
+--
+
+LOCK TABLES `entity_user` WRITE;
+/*!40000 ALTER TABLE `entity_user` DISABLE KEYS */;
+INSERT INTO `entity_user` VALUES ('a123321','123321','tie04021030@gmail.com'),('karel','Tie123321','karel@gmail.com'),('patrick','a123321','135dasd@qqcxa.cxz'),('tiewei','a123321','1030@gmail.com'),('tieweitong','a123321','tie04021030@gmail.com'),('tieweitong0402','a123321','tie04021030@gmail.com'),('tieweitong1','123321','tie0402@gmail.com'),('tieweitong10','a123321','tie04021030@qq.com'),('tieweitongaa','123321','tie04021030@gmail.com');
+/*!40000 ALTER TABLE `entity_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-02  1:34:56
