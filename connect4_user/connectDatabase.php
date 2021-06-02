84b511b0707e11e01bfd18b6eacfcc44fe965d05
TieWeitong_CIS17A_40366 <tie04021030@gmail.com>
2021-06-01 20:49:24 -0700

    06/01
    Code has been able to connect the database to register, log in to the account, and other functions. And still trying to able to turn the page on the same web while viewing all the user information on the administration(commented this part for right now)

--- connect4_user/connectDatabase.php
+++ connect4_user/connectDatabase.php
@@ -10,12 +10,11 @@
     $username = "root";
     $password = "";
     $dbname = "connect4";
-
+    
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-    } 
-    echo "Connected successfully";
+    }
 ?>
---
+++ connect4_user/login.php
@@ -0,0 +1,34 @@
+<?php
+    include'connectDatabase.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    $user = $_POST["user"];
+    $pw = $_POST["psw"];
+    
+    $checkbox_select=$_POST["category"];
+    $expire=time()+60*60*24*30;
+    if($checkbox_select==3){
+        setcookie("user_cookie", $user, $expire);
+        $ck = $_COOKIE["user_cookie"];
+    }
+    else{
+        setcookie("user_cookie", " ", $expire);
+    }
+    
+    if($user=='admin' && $pw=='admin'){
+       echo "<script> alert('Welcome to administration');window.location.href='admin.html'; </script>";
+    }
+    $query = "SELECT * FROM entity_user WHERE user_name='$user' and pass='$pw'";
+    $result = $conn->query($query);
+    
+    if($result->num_rows > 0){
+        echo "<script> alert('Login successful'); </script>";
+    }
+    else{
+        echo "<script> alert('Wrong username/password');window.location.href='index.php'; </script>";
+    }
+?>
